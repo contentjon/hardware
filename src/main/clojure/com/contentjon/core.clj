3a79cdc7da1bfd4a2df1b03ad84c65d01f058804
Jan Krueger <jan.krueger@gmail.com>
2011-03-14 17:15:10 +0100

    added function to dynamically require the appropriate backend based on the string returned by (System/getProperty "os.name")

--- src/main/clojure/com/contentjon/core.clj
+++ src/main/clojure/com/contentjon/core.clj
@@ -1,2 +1,12 @@
 (ns com.contentjon.core)
 
+(def implementations
+ { :Linux 'com.contentjon.udev })
+
+(defn require-os-implementation []
+  (let [os (System/getProperty "os.name")]
+    (if-let [implementation (implementations (keyword os))]
+      (require implementation)
+      (throw
+       (Exception.
+        (str "No implementation for operating system: " os))))))
