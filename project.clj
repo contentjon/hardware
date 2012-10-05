c446dc713c884591928f6cc3c5ab379e751aba4b
Jan Krueger <jan.krueger@gateprotect.com>
2012-10-05 11:34:45 +0200

    added leiningen build

---
+++ project.clj
@@ -0,0 +1,5 @@
+(defproject com.contentjon/hardware "0.0.5"
+  :dependencies [[org.clojure/clojure  "1.3.0"]
+                 [net.java.dev.jna/jna "3.2.4"]]
+  :source-paths ["src/main/clojure"]
+  :test-paths   ["src/test/clojure"])
