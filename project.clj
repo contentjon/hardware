11d8d89d24f5bb109e1468735deda8255ce2f287
Jan Krueger <jan.krueger@gateprotect.com>
2012-10-05 13:58:15 +0200

    fixed java source paths option

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject com.contentjon/hardware "0.0.6-SNAPSHOT"
+(defproject com.contentjon/hardware "1.0.0"
   :dependencies [[org.clojure/clojure  "1.3.0"]
                  [net.java.dev.jna/jna "3.2.4"]]
-  :source-paths     ["src/main/clojure"]
-  :java-source-path "src/main/java"
-  :test-paths       ["src/test/clojure"])
+  :source-paths      ["src/main/clojure"]
+  :java-source-paths ["src/main/java"]
+  :test-paths        ["src/test/clojure"])
