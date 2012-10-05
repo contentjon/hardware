f16850bddcacf0cf9f7f23d71afc8277062ce882
Jan Krueger <jan.krueger@gateprotect.com>
2012-10-05 13:45:44 +0200

    Compile java glue code to JNA as well

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
 (defproject com.contentjon/hardware "0.0.5"
   :dependencies [[org.clojure/clojure  "1.3.0"]
                  [net.java.dev.jna/jna "3.2.4"]]
-  :source-paths ["src/main/clojure"]
-  :test-paths   ["src/test/clojure"])
+  :source-paths     ["src/main/clojure"]
+  :java-source-path "src/main/java"
+  :test-paths       ["src/test/clojure"])
